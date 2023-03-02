d6b733aa9760b5e822d507b9ca5ce560889f4e3f
masa <masamaruna@gmail.com>
2023-03-02 14:59:48 +0100

    else if versus switch. testing and trying to understand the base logic.

---
+++ elseIfSwitch.js
@@ -0,0 +1,33 @@
+let familyMember = "Masa";
+
+switch (familyMember) {
+    case "Masa":
+        console.log("Hi Masa");
+        break;
+    case "Matej":
+        console.log("Hi Matej");
+        break;
+    case "Tomislav":
+        console.log("Hi Tomislav");
+        break;
+    case "Jasminka":
+        console.log("Hi Jasminka");
+        break;
+    default:
+        console.log("Not handeled");
+        break;
+}
+
+
+let name = "6";
+let nameLower = name.toLowerCase(); // ovo je irelevantno sada ali je prije imalo smisla it's just here now
+
+if (nameLower === "8") {
+    console.log("9");
+} else if (nameLower === "6") {
+    console.log("7");
+} else if (nameLower === "10") {
+    console.log("11");
+} else {
+    console.log("Not handeled");
+}
