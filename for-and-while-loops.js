cc484def212bf3c2b9b356872570511a807d83b6
masa <masamaruna@gmail.com>
2023-03-06 18:18:03 +0100

    learned the while loop!

--- for-and-while-loops.js
+++ for-and-while-loops.js
@@ -5,10 +5,36 @@ let loremIpsum =("Lorem Ipsum is simply dummy text of the printing and typesetti
     " it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic" +
     " typesetting, remaining essentially unchanged.").split(" ");
 
-for (let i = 0; 1 < loremIpsum.length; i++) {
+for (let i = 0; i < loremIpsum.length; i++) {
     const loremMail = loremIpsum[i].toLowerCase() + "@gmail.com";
 
-    console.log(loremMail);
+    console.log(i, loremMail);
 }
 
-// while loop
+
+// while loop v1
+
+let num = Number(prompt("Give me a number"));
+let startingNum = 1;
+while (startingNum <= num) {
+    console.log(startingNum);
+    startingNum++; // adds one to the previous number
+}
+
+
+// while loop v2
+
+const liElement = document.querySelector("#js-list-item");
+let html = " ";
+
+const numTwo = Number(prompt("give me a numebr"));
+let startingNumTwo = 1;
+while (startingNumTwo <= numTwo) {
+    html = html + "<li>" + startingNumTwo + "</li>";
+    startingNumTwo++;
+}
+
+liElement.innerHTML = html;
+
+
+// for each loop
