d26d15306470ed66d599c298df96bc396291bc5d
masa <masamaruna@gmail.com>
2023-03-13 21:30:51 +0100

    added another example for functions and also incorporating arrays

--- course-02/functions.js
+++ course-02/functions.js
@@ -4,4 +4,20 @@ function Addition(num1, num2) {
 
 const total1 = Addition(2, 45);
 const total2 = Addition(3,  58);
+
 console.log(total1 + total2);
+
+//
+
+function getName() {
+    const name = document.querySelector("input").value;
+    return(name);
+}
+
+function getFirstName() {
+    let nameArr = getName().split(" ");
+    return(nameArr[0]);
+}
+
+const firstName = getFirstName();
+console.log(firstName);
