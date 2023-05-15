e8cdbd783bf4f4d816d7116f061c62d08e4fbdbd
Masa <masamaruna@gmail.com>
2023-05-15 14:12:42 +0200

    added a small example for optional chaining

--- course-03/spicy-ifs.js
+++ course-03/spicy-ifs.js
@@ -13,6 +13,25 @@ const customer = {name:"John Doe", role:"pickup person", numberOfReservations: 1
 
 let isNew = customer.numberOfReservations === 1;
 console.log(isNew);
-
 // i still prefer if statements because they are much clearer
 
+
+// optional chaining
+
+const client = {
+    isNewUser : false,
+    userName : "Anna",
+    type : "admin",
+    // signUpData : {
+    //     signUpDate : "01.01.2000.",
+    //     signUpTime : "14:56",
+    //     originalEmail : "email@email.com",
+    //     signUpSource : {
+    //         browser : "Arc",
+    //         url : "https://..."
+    //     }
+    // }
+}
+console.log(client.signUpData?.signUpSource.browser);
+
+// basically what happens here is that we are missing some properties that would usually be there, so we add a question mark to let it "silently fail"
